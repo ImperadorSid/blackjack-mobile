3d14b8d50b613cc98687de15e69a66119e7aee5e
Moisés Carvalho <moisescarv27@hotmail.com>
2019-11-27 01:36:06 -0300

    Add and remove players

--- lib/game.dart
+++ lib/game.dart
@@ -10,24 +10,63 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
+  List<String> players = [];
+
+  void addPlayer(String playerName) {
+    setState(() => players.add(playerName));
+    Navigator.of(context).pop();
+  }
+
+  void removePlayer(int indexToRemove) =>
+      setState(() => players.removeAt(indexToRemove));
+
+  ListTile playerRow(int playerIndex) => ListTile(
+        
+        title: Text('${players[playerIndex]}'),
+        trailing: FlatButton(
+          onPressed: () => removePlayer(playerIndex),
+          child: Icon(Icons.cancel),
+        ),
+      );
+
+  void dialogInputPlayerName() {
+    String playerName = '';
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Adicionar Player'),
+        content:
+            TextField(onChanged: (nameInputted) => playerName = nameInputted),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('Cancelar'),
+          ),
+          FlatButton(
+              onPressed: () => addPlayer(playerName), child: Text('Adicionar')),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    print(players);
+
     return Scaffold(
-      appBar: AppBar(title: Text('Game')),
+      appBar: AppBar(title: Text('${widget.targetNumber}')),
       body: Column(
         children: <Widget>[
-          Text('${widget.targetNumber}'),
           Expanded(
-            child:
-                ListView.builder(itemBuilder: (context, index) => ListTile(
-                  title: TextField(),
-                  leading: Text('$index'),
-                )),
+            child: ListView.builder(
+              itemBuilder: (context, index) => playerRow(index),
+              itemCount: players.length,
+            ),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
+        onPressed: dialogInputPlayerName,
         child: Icon(Icons.add),
       ),
     );
